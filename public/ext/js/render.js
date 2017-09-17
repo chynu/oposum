e4e96bbd2dc26ae7920b77a9a81dd2c076b8c942
edusearch <celine.choo98@gmail.com>
2017-09-17 00:55:03 +0000

    Minor changes to render

--- public/ext/js/render.js
+++ public/ext/js/render.js
@@ -1,6 +1,9 @@
+// Waits for page to load. Temporary hack bc YT uses
+// asynchronous loading and idk how to do this lol
 setTimeout(loadButton, 2000);
 
-var opossum_clicked = false;
+// Whether the video has been clicked?
+var opossum_clicked = false; // TODO: Show as already saved after checking db
 var video_id = window.location.href.split('/watch?v=')[1];
 
 /**
@@ -39,7 +42,6 @@ function loadButton() {
  * Gets summary of currently playing video.
  */
 function getSummary() {
-  // TODO: Get summary of video currently playing.
   console.log('Retrieving summary...');
   var oSummary = '';
   $.ajax({
