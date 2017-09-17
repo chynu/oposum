ab14a9476e730f47141e3617b2646c8acd0cf9b2
Celine <cc972@cornell.edu>
2017-09-17 05:06:42 -0400

    Trying to make extension work

--- public/ext/js/summarize.js
+++ public/ext/js/summarize.js
@@ -7,34 +7,10 @@
  * AJAX post and displays into the extension window.
  */
 
-// Gets URL of currently open tab.
-// window.onload = function(){
-//   chrome.runtime.getBackgroundPage(function(backgroundPage){ 
-//     var currentUrl = backgroundPage.tabURL;
-//     var id = getId(currentUrl);
-//     console.log('Current URL: ' + currentUrl);
-
-//     // TODO: Replace video ID with the ID of the current video.
-//     getSummary(id, function(data) {
-//       console.log('Here is the summary.');
-//       var sum = data || 'Sample text.';
-//       $('#summary').html(data);
-//     });
-//   });
-// };
-
-/**
- * Gets summary of currently playing video.
- */
-// function getSummary(video_id, grabText) {
-//   console.log('Retrieving summary...');
-//   $.ajax({
-//     method: 'POST',
-//     url: 'https://edusearch-eleng555.c9users.io/' + video_id,
-//     dataType: 'text',
-//     success: function(data) {
-//       console.log('Summary retrieved!');
-//       grabText(data);
-//     }
-//   });
-// }
+// When clicking the button to go to saved summaries.
+$('#saved').click(function() {
+    var newURL = "https://google.com/";
+    chrome.tabs.create({ 
+      url: 'http://google.com' 
+    }, function(){});
+});
